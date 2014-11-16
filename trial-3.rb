06032cd74c282753a9141826b551c617cdf9132c
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2014-11-16 23:22:08 +0900

    Improve trial-3

--- trial-3.rb
+++ trial-3.rb
@@ -1,4 +1,11 @@
-require 'unimidi'
+$outputBin = false
+$outputBin = true if ARGV.length == 1
+
+if $outputBin
+  $file = File::open("sample_midi_stream.bin", "wb")
+elsif
+  require 'unimidi'
+end
 
 $note_numbers = [62,64,65,67,69,71,72]
 $note_lengths = [0.25, 0.25, 0.25, 1.25]
@@ -19,26 +26,47 @@ def next_note_index(last_index)
   return last_index
 end
 
-UniMIDI::Output.gets do |output|
-  begin
-    srand(0)
-    last_index = 0
-    q = []
-    q.push(Note.new(last_index, $note_lengths[-1]))
-
-    loop do
-      note = q.shift
-      if note
-        output.puts(0x90, $note_numbers[note.number], 100)
-        sleep($duration * note.time)
-        output.puts(0x80, $note_numbers[note.number], 100)
-        sleep((1.0 - $duration) * note.time)
-      else
-        $note_lengths.each do |note_length|
-          last_index = next_note_index(last_index)
-          q.push(Note.new(last_index, note_length))
-        end
+def main_loop
+  if $outputBin
+    (0..63).each do
+        yield
+    end
+  else
+    UniMIDI::Output.gets do |output|
+      loop do
+        yield(output)
       end
+    end
+  end
+end
+
+def play(note, output)
+  if $outputBin
+    $file.write([0x90, $note_numbers[note.number], 100].pack("C*"))
+    (($duration * note.time) * 4000).to_i.times { $file.write([0xFE].pack("C")) }
+    $file.write([0x80, $note_numbers[note.number], 100].pack("C*"))
+    (((1.0 - $duration) * note.time) * 4000).to_i.times { $file.write([0xFE].pack("C")) }
+  elsif
+    output.puts(0x90, $note_numbers[note.number], 100)
+    sleep($duration * note.time)
+    output.puts(0x80, $note_numbers[note.number], 100)
+    sleep((1.0 - $duration) * note.time)
+  end
+end
+
+srand(0)
+last_index = 0
+q = []
+q.push(Note.new(last_index, $note_lengths[-1]))
+
+main_loop do |output|
+  note = q.shift
+  if note
+    play(note, output)
+  else
+    $note_lengths.each do |note_length|
+      last_index = next_note_index(last_index)
+      q.push(Note.new(last_index, note_length))
     end
   end
 end
